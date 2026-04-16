f8b2fb508131b4d029164115298f2141262e41cf
Alexey Fedorov <Aleksey-Fedorov@mailfrom.ru>
2026-04-16 22:12:06 +0300

    Sprint 7

--- sprint6_pandas/pandas_test.py
+++ sprint6_pandas/pandas_test.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 """
@@ -54,10 +56,52 @@ print(df)
 print(type(df['column_int'][2]))
 """
 
-data = {
-    'product': ['Хлеб', 'Молоко', 'Сыр', 'Хлеб', 'Сыр'],
-    'count': [20, 30, 15, 25, 20],
-    'price': [50, 60, 200, 55, 210]
-}
-df = pd.DataFrame(data)
-print(df)
+# data = {
+#     'product': ['Хлеб', 'Молоко', 'Сыр', 'Хлеб', 'Сыр'],
+#     'count': [20, 30, 15, 25, 20],
+#     'price': [50, 60, 200, 55, 210]
+# }
+# df = pd.DataFrame(data)
+# print(df)
+
+
+# # The `people` DataFrame
+# people = pd.DataFrame({'FirstName': ['John', 'Jane'],
+#                        'LastName': ['Doe', 'Austen'],
+#                        'BloodType': ['A-', 'B+'],
+#                        'Weight': [90, 64]})
+
+# print(people)
+
+# # Use `melt()` on the `people` DataFrame
+# print(pd.melt(people, id_vars=['FirstName', 'LastName'], var_name='measurements'))
+
+
+# Создаём датафрейм с названиями фруктов
+fruits = pd.DataFrame({
+    'fruit_id': [1, 2, 3],
+    'fruit_name': ['Яблоки', 'Груши', 'Бананы']
+})
+
+# Создаём датафрейм с продажами фруктов
+# fruits_sale = pd.DataFrame({
+#     'cheque_id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+#     'fruit_id': [1, 2, 3, 3, 2, 1, 2, 1, 3, 3, 1, 2],
+#     'amount': [11, 22, 26, 18, 12, 34, 57, 15, 36, 25, 30, 31]
+# })
+
+# sum_amount = fruits_sale.groupby('fruit_id')['amount'].sum()
+
+# print(type(sum_amount))
+# print("Индекс:", sum_amount.index)
+# print("Индекс:", sum_amount.index.name)
+# print(sum_amount)
+
+
+# Загружаем bank_information.csv в переменную df
+df = pd.read_csv('https://code.s3.yandex.net/datasets/bank_information.csv')
+
+# Строим гистограммы по всем данным датафрейма
+df.hist(figsize=(12, 10))
+plt.tight_layout()
+plt.show()
